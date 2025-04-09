b31fb5dafc484b2e35b8a4e3aadc4d87b94ca861
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2025-04-08 19:17:04 -0600

    Sí jala el privacy

--- obj/Debug/net9.0/Menus.AssemblyInfo.cs
+++ obj/Debug/net9.0/Menus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Menus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b7a03a489585dfd8ef9bea906d5e1cbd2933454")]
 [assembly: System.Reflection.AssemblyProductAttribute("Menus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Menus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
