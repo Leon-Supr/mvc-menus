3b56429b3b15750f4bee893b9c873d9f8f7bbaa2
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2025-04-22 20:00:06 -0600

    Add /GetPokemon/example endpoint
    When you search localhost/Home/GetPokemon/Pikachu, it returns the
    pokemon stats, still lacking styles

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,57 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Menus.Models;
+using System.Text.Json;
+using menus.Models;
 
 namespace Menus.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    [HttpGet("Home/GetPokemon/{name}")]
+    public async Task<IActionResult> GetPokemon(string name)
     {
-        _logger = logger;
+        String url = $"https://pokeapi.co/api/v2/pokemon/{name}";
+        HttpResponseMessage response =
+             await httpClient.GetAsync(url);
+        if (!response.IsSuccessStatusCode)
+        {
+            return NotFound();
+        }
+        String json = await response
+                           .Content
+                           .ReadAsStringAsync();
+        JsonDocument doc = JsonDocument
+                          .Parse(json);
+        Pokemon pokemon = new Pokemon
+        {
+            Name = doc.RootElement.GetProperty("name").GetString(),
+            ImageUrl = doc
+                   .RootElement
+                   .GetProperty("sprites")
+                   .GetProperty("front_default")
+                   .GetString(),
+            Types = [.. doc.RootElement.GetProperty("types")
+           .EnumerateArray()
+           .Select(t => t.GetProperty("type")
+                       .GetProperty("name")
+                       .GetString())],
+            Abilities = [.. doc.RootElement.GetProperty("abilities")
+                .EnumerateArray()
+                .Select(a => a.GetProperty("ability")
+                              .GetProperty("name")
+                              .GetString())],
+            Weight = doc.RootElement.GetProperty("weight").GetInt32(),
+        };
+
+        return View(pokemon);
+    }
+    private readonly HttpClient httpClient;
+
+    public HomeController(HttpClient httpClient)
+    {
+        this.httpClient = new HttpClient();
     }
 
     public IActionResult Index()
@@ -37,4 +78,5 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
 }
